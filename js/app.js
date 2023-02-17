0a4685e6165fda37a96dee0916e92c28bcda7c09
sajim-khan <shossain1427@gmail.com>
2023-02-17 16:15:06 +0600

    added function for pin in display

---
+++ js/app.js
@@ -0,0 +1,24 @@
+function getPin() {
+    const pin = generatePin();
+    const pinString = pin + '';
+    if (pinString.length === 4){
+        return pin;
+    } else {
+        console.log('pin 3 digit found', pin)
+        return getPin();
+    }
+}
+
+
+function generatePin() {
+    const random = Math.round(Math.random()*10000);
+    return random;
+}
+
+document.getElementById('generate-pin').addEventListener('click', function () {
+    const pin = getPin();
+    //console.log(pin)
+    
+    const displayPinField = document.getElementById('display-pin')
+    displayPinField.value = pin;
+});
